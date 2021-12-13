fc749732c78028f928b46bc4f05ed04f983fc9ed
Isaiah Murray <murray.isaiah13@gmail.com>
2021-12-13 18:28:17 -0500

    Removed project card and updated projects sections

--- src/components/ProjectComponents/CSProjects.jsx
+++ src/components/ProjectComponents/CSProjects.jsx
@@ -1,94 +1,175 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Zoom from "@material-ui/core/Zoom";
-import ProjectCard from "./ProjectCard";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-
-//image imports
-import note from "../../assets/notepad.png";
-import sounds from "../../assets/sounds.png";
-import LC from '../../assets/LC.png';
+import {
+  Zoom,
+  Paper,
+  Box,
+  Modal,
+  Button,
+  Fade,
+  Typography,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  CardActions,
+} from "@mui/material/";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
   paper: {
     margin: theme.spacing(3),
     padding: theme.spacing(2),
     textAlign: "center",
+    justifyContent: "center",
     color: theme.palette.text.secondary,
   },
   container: {
     display: "flex",
     flexWrap: "wrap",
-    justifyContent: "center"
-  },
-  card: {
-    maxWidth: 345,
+    justifyContent: "center",
   },
   media: {
     height: 160,
   },
+  card: {
+    width: 200,
+    height: 300,
+  },
+  content: {
+    height: 100
+  },
+  modal: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 600,
+    padding: 25,
+    height: 450,
+  },
 }));
 
-const elevenNoteProject = {
-  title: "Eleven Note",
-  description: "A simple note taking application implementing full CRUD.",
-  image: note,
-  code: "https://github.com/IsaiahMurray/ElevenNote",
-};
-
-const colossalSounds = {
-  title: "Colossal Sounds",
-  description:
-    "A database management app for a music store owner that can also function as a way for customers to view products available for sale at the store, and their essential information.",
-  code: "https://github.com/IsaiahMurray/Colossal-sounds",
-  image: sounds,
-};
-
-const lilChef = {
-  title: "Lil Chef",
-  description: 'A web application for creating and managing recipes and ingredients for any who would find it usefull. Built using ASP.NET MVC for front and back end functionality.',
-  code: 'https://github.com/IsaiahMurray/LilChef',
-  image: LC,
-}
-
-const CSProjects = () => {
+const CSProjects = (props) => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [active, setActive] = useState({});
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const activate = (project) => {
+    setActive(project);
+    handleOpen();
+  };
+
+  const displayProjects = () => {
+    return props.projects.map((project, index) => {
+      return (
+        <div>
+          <Zoom
+            style={{ transitionDelay: `${index}00ms` }}
+            in={true}
+            mountOnEnter
+            unmountOnExit
+          >
+            <Paper elevation={4} className={classes.paper}>
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={project.image}
+                    title={project.title}
+                  />
+                  <CardContent className={classes.content}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {project.title}
+                    </Typography>
+                  </CardContent>
+                  <Button onClick={() => activate(project)}>Details</Button>
+                </CardActionArea>
+              </Card>
+            </Paper>
+          </Zoom>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Fade in={open}>
+              <Card className={classes.modal}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={active.image}
+                    title={active.title}
+                  />
+                  <CardContent className={classes.content}>
+                    <Typography
+                      id="transition-modal-title"
+                      variant="h6"
+                      component="h2"
+                    >
+                      {active.title}
+                    </Typography>
+                    <Typography
+                      id="transition-modal-description"
+                      sx={{ mt: 2 }}
+                    >
+                      {active.description}
+                    </Typography>
+                    <CardActions>
+                      {active.page ? (
+                        <Button>
+                          <a href={active.page} target="_blank">
+                            Page
+                          </a>
+                        </Button>
+                      ) : null}
+
+                      <br />
+                      {active.repo ? (
+                        <Button>
+                          <a href={active.repo} target="_blank">
+                            Github Repo
+                          </a>
+                        </Button>
+                      ) : null}
+
+                      <br />
+                      {active.client ? (
+                        <Button>
+                          <a href={active.client} target="_blank">
+                            Github Client Repo
+                          </a>
+                        </Button>
+                      ) : null}
+
+                      <br />
+                      {active.server ? (
+                        <Button>
+                          <a href={active.server} target="_blank">
+                            Github Server Repo
+                          </a>
+                        </Button>
+                      ) : null}
+
+                      <br />
+                    </CardActions>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Fade>
+          </Modal>
+        </div>
+      );
+    });
+  };
+
   return (
     <div>
       <div className={classes.container}>
         <Box display="flex" flexWrap="wrap" justifyContent="center">
-          <Zoom in={true} mountOnEnter unmountOnExit>
-            <Paper elevation={4} className={classes.paper}>
-              <ProjectCard {...elevenNoteProject} />
-            </Paper>
-          </Zoom>
-
-          <Zoom
-            style={{ transitionDelay: " 100ms" }}
-            in={true}
-            mountOnEnter
-            unmountOnExit
-          >
-            <Paper elevation={4} className={classes.paper}>
-              <ProjectCard {...colossalSounds} />
-            </Paper>
-          </Zoom>
-
-          <Zoom
-            style={{ transitionDelay: " 200ms" }}
-            in={true}
-            mountOnEnter
-            unmountOnExit
-          >
-            <Paper elevation={4} className={classes.paper}>
-              <ProjectCard {...lilChef} />
-            </Paper>
-          </Zoom>
+          {displayProjects()}
         </Box>
       </div>
     </div>
--- src/components/ProjectComponents/OtherProjects.jsx
+++ src/components/ProjectComponents/OtherProjects.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Zoom from "@material-ui/core/Zoom";
-import ProjectCard from "./ProjectCard";
-import Paper from "@material-ui/core/Paper";
-import Box from '@material-ui/core/Box';
-
-//image import
-import sharingan from '../../assets/sharingan.png'
+import {
+  Zoom,
+  Paper,
+  Box,
+  Modal,
+  Button,
+  Fade,
+  Typography,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  CardActions,
+} from "@mui/material/";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
   paper: {
     margin: theme.spacing(3),
     padding: theme.spacing(2),
@@ -23,60 +26,153 @@ const useStyles = makeStyles((theme) => ({
   container: {
     display: "flex",
     flexWrap: "wrap",
-    justifyContent: "center"
-  },
-  card: {
-    maxWidth: 345,
+    justifyContent: "center",
   },
   media: {
     height: 160,
   },
+  card: {
+    width: 200,
+    height: 300,
+  },
+  content: {
+    height: 100
+  },
+  modal: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 600,
+    padding: 25,
+    height: 450,
+  },
 }));
 
-const sharinganProject = {
-  title: 'Transforming Sharingan',
-  description: 'A fun little CSS project I did for my gold badge at Eleven Fifty Academy. I am a big Naruto fan so I really wanted to do this one.',
-  code: 'https://codepen.io/Salvation205/pen/QWypRVY',
-  image: sharingan,
-}
-
-const OtherProjects = () => {
+const OtherProjects = (props) => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [active, setActive] = useState({});
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const activate = (project) => {
+    setActive(project);
+    handleOpen();
+  };
+
+  const displayProjects = () => {
+    return props.projects.map((project, index) => {
+      return (
+        <div>
+          <Zoom
+            style={{ transitionDelay: `${index}00ms` }}
+            in={true}
+            mountOnEnter
+            unmountOnExit
+          >
+            <Paper elevation={4} className={classes.paper}>
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={project.image}
+                    title={project.title}
+                  />
+                  <CardContent className={classes.content}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {project.title}
+                    </Typography>
+                  </CardContent>
+                  <Button onClick={() => activate(project)}>Details</Button>
+                </CardActionArea>
+              </Card>
+            </Paper>
+          </Zoom>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Fade in={open}>
+              <Card className={classes.modal}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={active.image}
+                    title={active.title}
+                  />
+                  <CardContent className={classes.content}>
+                    <Typography
+                      id="transition-modal-title"
+                      variant="h6"
+                      component="h2"
+                    >
+                      {active.title}
+                    </Typography>
+                    <Typography
+                      id="transition-modal-description"
+                      sx={{ mt: 2 }}
+                    >
+                      {active.description}
+                    </Typography>
+                    <CardActions>
+                      {active.page ? (
+                        <Button>
+                          <a href={active.page} target="_blank">
+                            Page
+                          </a>
+                        </Button>
+                      ) : null}
+
+                      <br />
+                      {active.repo ? (
+                        <Button>
+                          <a href={active.repo} target="_blank">
+                            Github Repo
+                          </a>
+                        </Button>
+                      ) : null}
+
+                      <br />
+                      {active.client ? (
+                        <Button>
+                          <a href={active.client} target="_blank">
+                            Github Client Repo
+                          </a>
+                        </Button>
+                      ) : null}
+
+                      <br />
+                      {active.server ? (
+                        <Button>
+                          <a href={active.server} target="_blank">
+                            Github Server Repo
+                          </a>
+                        </Button>
+                      ) : null}
+
+                      <br />
+                    </CardActions>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Fade>
+          </Modal>
+        </div>
+      );
+    });
+  };
+
   return (
     <div>
-    <div className={classes.container}>
-      <Box display="flex" flexWrap="wrap" justifyContent="center" >
-
-      <Zoom in={true} mountOnEnter unmountOnExit>
-        <Paper elevation={4} className={classes.paper}>
-          <ProjectCard {...sharinganProject}/>
-        </Paper>
-      </Zoom>
-
-      {/* <Zoom
-        style={{ transitionDelay: " 100ms" }}
-        in={true}
-        mountOnEnter
-        unmountOnExit
-      >
-        <Paper elevation={4} className={classes.paper}>
-          <ProjectCard />
-        </Paper>
-      </Zoom>
-
-      <Zoom
-        style={{ transitionDelay: " 200ms" }}
-        in={true}
-        mountOnEnter
-        unmountOnExit
-      >
-        <Paper elevation={4} className={classes.paper}>
-          <ProjectCard />
-        </Paper>
-      </Zoom> */}
-      </Box>
+      <div className={classes.container}>
+        <Box display="flex" flexWrap="wrap" justifyContent="center">
+          {displayProjects()}
+        </Box>
+      </div>
     </div>
-  </div>
   );
 };
 
